bcfd3883723e5d41ace9e69b20b765db1d03b00f
taheeronline <taheeronline@outlook.com>
2019-05-09 18:21:50 +0530

    Swagger Functioning  as part of project

--- TRMDataManager/App_Start/SwaggerConfig.cs
+++ TRMDataManager/App_Start/SwaggerConfig.cs
@@ -2,7 +2,9 @@ using System.Web.Http;
 using WebActivatorEx;
 using TRMDataManager;
 using Swashbuckle.Application;
+using TRMDataManager.App_Start;
 
+//Diable this line to disable swagger for production deployment
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
 namespace TRMDataManager
@@ -16,6 +18,8 @@ namespace TRMDataManager
             GlobalConfiguration.Configuration
                 .EnableSwagger(c =>
                     {
+                        c.DocumentFilter<AuthTokenOperation>();
+                        c.OperationFilter<AuthorizationOperationFilter>();
                         // By default, the service root url is inferred from the request used to access the docs.
                         // However, there may be situations (e.g. proxy and load-balanced environments) where this does not
                         // resolve correctly. You can workaround this by providing your own code to determine the root URL.
@@ -36,7 +40,7 @@ namespace TRMDataManager
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
-                        //c.PrettyPrint();
+                        c.PrettyPrint();
 
                         // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
                         // In this case, you must provide a lambda that tells Swashbuckle which actions should be
@@ -182,7 +186,7 @@ namespace TRMDataManager
                         // Use the "DocumentTitle" option to change the Document title.
                         // Very helpful when you have multiple Swagger pages open, to tell them apart.
                         //
-                        //c.DocumentTitle("My Swagger UI");
+                        c.DocumentTitle("TRM Data Manager");
 
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
